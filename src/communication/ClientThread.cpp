244d70ade2d22e14ff62bb05add73db8b1ec2ad2
Oleg <oleg.tarassov@guavus.com>
2015-02-06 01:15:48 -0500

    Added client thread

---
+++ src/communication/ClientThread.cpp
@@ -0,0 +1,55 @@
+/*
+ * ClientThread.cpp
+ *
+ *      Author: AUV Capstone
+ */
+
+#include "ClientThread.h"
+
+//-----------------------------------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------------------------------
+ClientThread::ClientThread()
+{
+	setThreadId( SERVER_THREAD_ID );
+	client = new SocketClient();
+
+}
+
+ClientThread::ClientThread( int port , char *ip)
+{
+	setThreadId( SERVER_THREAD_ID );
+	client = new SocketClient( port, ip );
+
+}
+//-----------------------------------------------------------------------------------------
+// Destructor
+//-----------------------------------------------------------------------------------------
+ClientThread::~ClientThread()
+{
+	delete client;
+}
+//-----------------------------------------------------------------------------------------
+// Overrides BaseThread's run() method
+//-----------------------------------------------------------------------------------------
+void* ClientThread::run()
+{
+	printf("[KPI::THREAD] START\n");
+	client->start();
+
+	client->recvMsg();
+
+	return NULL;
+}
+
+int ClientThread::stop()
+{
+	printf("[KPI::THREAD] STOP\n");
+	return pthread_cancel( CLIENT_THREAD_ID );
+}
+
+int ClientThread::kill()
+{
+	printf("[KPI::THREAD] KILL\n");
+	return pthread_kill( CLIENT_THREAD_ID , SIGQUIT );
+}
