67583a3cc43d667e5061771adf3122ae410f6962
Oleg <oleg.tarassov@guavus.com>
2015-02-07 23:41:43 -0500

    Server main clean

--- src/auv.cpp
+++ src/auv.cpp
@@ -15,45 +15,26 @@ int main(int argc, char *argv[])
 {
 	printf("[KPI::MAIN] START\n");
 	ServerThread* serverThread = new ServerThread( 5000, 2);
-	ClientThread* clientThread = new ClientThread();
-
-	FdTimer *delayTimer;
-	delayTimer = new FdTimer(10, 3);
-
 
 	serverThread->start();
-
-	delayTimer->startSingle();
-	delayTimer->waitTimerEvent();
-
-	clientThread->start();
 
     char key;
     do
     {
-
         key = getchar();
-        //clientThread->recv();
 
     }while(key != ESC);
 
-    printf("[KPI::SERVER]::Escape Character Triggered\n");
+    printf("[KPI::MAIN]::Escape Character Triggered\n");
 
     if( serverThread->stop() != 0 )
     {
     	printf("[KPI::MAIN] failed stop\n");
-
     	serverThread->kill();
     }
-    //serverThread->join();
 
     delete serverThread;
-    delete clientThread;
-    delete delayTimer;
-
     printf("[KPI::MAIN] END\n");
-
-
 
     return EXIT_SUCCESS;
 }
