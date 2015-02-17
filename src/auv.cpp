337ca2f74da27a9a0a289b1658f8bab9071f2a7a
Oleg <oleg.tarassov@guavus.com>
2015-02-16 23:39:09 -0500

    Added main to work for both client or server socket

--- src/auv.cpp
+++ src/auv.cpp
@@ -4,6 +4,7 @@
 //============================================================================
 
 #include "communication/ServerThread.h"
+#include "src/communication/ClientThread.h"
 
 #include "imu/ImuThread.h"
 #include "dmu/DmuThread.h"
@@ -21,41 +22,65 @@ int main(int argc, char *argv[])
 {
 	syslog(LOG_NOTICE,"[KPI::MAIN] START");
 
-	SensorData* sensorData = new SensorData();
-	DesiredData* targetData = new DesiredData();
-	CameraData* camData = new CameraData();
-
-	//DEBUGImuThread* imuThread = new ImuThread(sensorData);
-	//DmuThread* dmuThread = new DmuThread(sensorData);
-
-	DataService* dataService = new DataService(sensorData, targetData, camData);
-
-	ServerThread* serverThread = new ServerThread( 5000, 2 , dataService);
-
-	serverThread->start();
-
-	//DEBUGimuThread->start();
-	//dmuThread->start();
-
-    char key;
-    do
+	//server start
+    if( argc == 0 || strcmp(argv[1], "server") )
     {
-        key = getchar();
+		SensorData* sensorData = new SensorData();
+		DesiredData* targetData = new DesiredData();
+		CameraData* camData = new CameraData();
 
-    }while(key != ESC);
+		//DEBUGImuThread* imuThread = new ImuThread(sensorData);
+		//DmuThread* dmuThread = new DmuThread(sensorData);
 
-    syslog(LOG_NOTICE,"[KPI::MAIN]::Escape Character Triggered");
+		DataService* dataService = new DataService(sensorData, targetData, camData);
+
+		ServerThread* serverThread = new ServerThread( 5000, 2 , dataService);
+
+		serverThread->start();
+
+		//DEBUGimuThread->start();
+		//dmuThread->start();
+
+		char key;
+		do
+		{
+			key = getchar();
+
+		}while(key != ESC);
+
+		syslog(LOG_NOTICE,"[KPI::MAIN]::Escape Character Triggered");
 
 
-    delete serverThread;
-    delete sensorData;
-    delete targetData;
-    delete camData;
-    delete dataService;
-    //delete dmuThread;
-    //DEBUGdelete imuThread;
+		delete serverThread;
+		delete sensorData;
+		delete targetData;
+		delete camData;
+		delete dataService;
+		//delete dmuThread;
+		//DEBUGdelete imuThread;
+    }
+    else if (strcmp(argv[1], "client"))
+    {
+    	ClientThread* clientThread = new ClientThread(5000, "192.168.10.50");
+    	clientThread->start();
+
+    	char key;
+    	do
+    	{
+    		key = getchar();
+
+    	}while(key != ESC);
+
+    	syslog(LOG_NOTICE,"[KPI::MAIN]::Escape Character Triggered");
+
+    	delete clientThread;
+    }
+    else
+    {
+    	syslog(LOG_ERR,"[KPI::MAIN] Wrong Options entered, possible ones are : client or server");
+    	return EXIT_FAILURE;
+    }
 
     syslog(LOG_NOTICE,"[KPI::MAIN] END");
-
     return EXIT_SUCCESS;
 }
